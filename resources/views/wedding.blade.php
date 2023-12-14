f845f5b1be9a226c72cdea5686d9bd27a43f3d16
@aamutezar <aamutezar@gmail.com>
2023-12-14 14:06:21 +0700

    update viewport

--- resources/views/wedding.blade.php
+++ resources/views/wedding.blade.php
@@ -282,7 +282,7 @@
     </div>
 
     <div class="vh-100 d-flex align-items-center">
-        <h1 class="d-none d-md-block d-lg-block d-xl-block d-xxl-none">This invitation wedding is only available on mobile devices</h1>
+        <h1 class="d-none d-sm-block">This invitation wedding is only available on mobile devices</h1>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
